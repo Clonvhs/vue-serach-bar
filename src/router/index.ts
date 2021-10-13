bf5100fbd461e19226fae267bbb20ef94e75d416
Clon <clonvhs@gmail.com>
2021-10-13 13:30:08 +0300

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Search from "../views/Search.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/search',
     name: 'Search',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue')
+    component:Search
   }
 ]
 
